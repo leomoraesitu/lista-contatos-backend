c61396ded3a635b735c7a767482c530472958aa4
leomoraesitu <leomoraesitu@hotmail.com>
2023-12-02 12:01:25 -0300

    start

---
+++ src/main/java/br/com/fatec/listacontatosbackend/ListaContatosBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.fatec.listacontatosbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaContatosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaContatosBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fatec/listacontatosbackend/ListaContatosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fatec.listacontatosbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaContatosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
